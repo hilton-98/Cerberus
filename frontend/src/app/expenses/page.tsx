30dae193bbf2bbe2dbed9813561cce85c524784c
Kevin Hilton <hiltonk98@gmail.com>
2024-06-19 16:33:11 -0600

    Add prettier code formatter for frontend

--- frontend/src/app/expenses/page.tsx
+++ frontend/src/app/expenses/page.tsx
@@ -1,32 +1,32 @@
-"use client";
+'use client';
 
 import { useEffect, useState } from 'react';
 
 export default function Expenses() {
-    const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState([]);
 
-   const loadExpenses: () => Promise<void> = async () => {
-      try {
-         const response = await fetch('http://localhost:8080/api/expenses');
-         const responseJson = await response.json();
-         setExpenses(responseJson);
-      } catch (e) {
-         console.log(e);
-      }
-   }
+  const loadExpenses: () => Promise<void> = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/expenses');
+      const responseJson = await response.json();
+      setExpenses(responseJson);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    useEffect(() => {
-      loadExpenses();
-    }, []);
+  useEffect(() => {
+    loadExpenses();
+  }, []);
 
-    return (
-        <div>
-            <h1>Expenses</h1>
-            <ul>
-                {expenses.map((expense, index) => (
-                    <li key={index}>{expense}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Expenses</h1>
+      <ul>
+        {expenses.map((expense, index) => (
+          <li key={index}>{expense}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
