66c53c221f6266356985036e67747e0d2f0683a7
Tanner Pearson <pearsontanner345@gmail.com>
2024-06-21 17:45:53 -0500

    add get user by id

--- backend/src/main/java/com/expense_hound/backend/Controller.java
+++ backend/src/main/java/com/expense_hound/backend/Controller.java
@@ -2,9 +2,13 @@ package com.expense_hound.backend;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +40,16 @@ public class Controller {
 	@PostMapping("/createUser")
 	public User createUser(@RequestBody User user) {
 		return userService.saveUser(user);
+	}
+
+	@GetMapping("/user/{id}")
+	public ResponseEntity<Object> getUserById(@PathVariable Integer id) {
+		Optional<User> user = userService.getUserById(id);
+
+		if (user.isPresent()) {
+			return ResponseEntity.ok(user.get());
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id: " + id);
+		}
 	}
 }
--- backend/src/main/java/com/expense_hound/backend/service/UserService.java
+++ backend/src/main/java/com/expense_hound/backend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.expense_hound.backend.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,10 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
     }
 
     public User saveUser(User user) {
