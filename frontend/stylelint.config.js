182811c6101b9973f1830728bc2aa407bd3693ea
Kevin Hilton <hiltonk98@gmail.com>
2024-06-24 21:31:28 -0600

    Add stylelinter

---
+++ frontend/stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-order', './customPlugins/stylelintExpenseHound'],
+  rules: {
+    'prettier/prettier': true,
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-alphabetical-order': true,
+    'no-empty-source': null, // Disables the no-empty-source rule
+    'plugin/expense-hound': [true, { fix: true }],
+  },
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+};
