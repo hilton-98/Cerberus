6d6fcc0f3cf578e6824af6afe1135abec1729462
Kevin Hilton <hiltonk98@gmail.com>
2024-07-15 19:29:24 -0600

    Fix CSS on login

--- frontend/src/app/expenses/page.tsx
+++ frontend/src/app/expenses/page.tsx
@@ -22,6 +22,7 @@ export default function Expenses() {
   };
 
   useEffect(() => {
+    console.log('Loading expenses');
     loadExpenses();
   }, []);
 
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,37 +3,48 @@
 import { useState } from 'react';
 import styles from './login.module.scss';
 
+const cssStyles = {
+  container: styles['container'],
+  inputContainer: styles['input-container'],
+};
+
+const phrases = {
+  passwordLabel: 'Password:',
+  submitButton: 'Submit',
+  title: 'Login',
+  usernameLabel: 'Username:',
+};
+
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const onSubmit = () => {
+    console.log({
+      username,
+      password,
+    });
+  }
+
   return (
-    <div className={styles['container']}>
-      Login
-      <label className={styles['input-container']}>
-        Username:
+    <div className={cssStyles.container}>
+      {phrases.title}
+      <label className={cssStyles.inputContainer}>
+        {phrases.usernameLabel}
         <input
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
       </label>
-      <label className={styles['input-container']}>
-        Password:
+      <label className={cssStyles.inputContainer}>
+        {phrases.passwordLabel}
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button
-        onClick={() => {
-          let info = {
-            username,
-            password,
-          };
-          console.log(info);
-        }}
-      >
-        Submit
+      <button onClick={onSubmit} >
+        {phrases.submitButton}
       </button>
     </div>
   );
